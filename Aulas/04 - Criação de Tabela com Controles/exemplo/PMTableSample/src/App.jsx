6224e3cde8748449b25786b76844374751c5d42d
tadeumaffeis <tadeu.maffeis@gmail.com>
2024-04-05 16:59:15 -0300

    Alteração 05-04-2024

--- Aulas/04 - Criação de Tabela com Controles/exemplo/PMTableSample/src/App.jsx
+++ Aulas/04 - Criação de Tabela com Controles/exemplo/PMTableSample/src/App.jsx
@@ -1,15 +1,19 @@
 
 import './App.css'
-import PMTableWithoutChildrenComponenets from './Components/PMTableWithoutChildrenComponents'
-import TableInformation from './Data/reservations.array.json';
+import ExampleParent from './Components/HookTest';
+//import PMTableWithoutChildrenComponenets from './Components/PMTableWithoutChildrenComponents'
+//import TableInformation from './Data/reservations.array.json';
 
 function App() {
   return (
+    /*
       <PMTableWithoutChildrenComponenets
         caption={TableInformation.caption}
         header={TableInformation.header}
         data={TableInformation.data}
       />
+      */
+     <ExampleParent />
   );
 }
 
---
+++ Aulas/04 - Criação de Tabela com Controles/exemplo/PMTableSample/src/Components/HookTest.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import { PropTypes } from 'prop-types';
+
+export function ExampleHook({ layout, prelayout }) {
+    console.log(React.usetState());
+    React.useLayoutEffect(() => {
+        const table = document.getElementsByTagName('table')[0];
+        console.log('useLayoutEffect', ' ', table.offsetWidth);
+        if (layout) {
+            table.style.width = '700px';
+        }
+        if (prelayout) {
+            table.style.width = '500px';
+            table.style.height = '20px';
+        }
+        const tableSize = document.getElementById('size');
+        tableSize.innerHTML = `Table width: ${table.offsetWidth}`;
+    }, [layout, prelayout]);
+    //React.useEffect(() => {
+    //    const table = document.getElementsByTagName('table')[0];
+    //    console.log('useEffect', ' ', table.offsetWidth);
+    //    if (layout) {
+    //        table.style.width = '130%';
+    //    }
+    //    if (prelayout) {
+    //        table.style.width = '50%';
+    //    }
+    //}, [layout, prelayout]);
+    return (
+        <table border="1" style={{ }}>
+            <thead>
+                <tr>
+                    <th>Random</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Array.from(Array(10)).map((_, idx) => (
+                    <tr key={idx}>
+                        <td width={Math.random() * 800}>{Math.random()}</td>
+                    </tr>
+                ))}
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td>
+                        <b>
+                            <span id="size"></span>
+                        </b>
+                    </td>
+                </tr>
+            </tfoot>
+        </table>
+    );
+}
+
+export default function ExampleParent() {
+    const [layout, setLayout] = React.useState(null);
+    const [prelayout, setPreLayout] = React.useState(null);
+    return (
+        <div>
+            <button onClick={() => { setLayout(true), setPreLayout(false) }}>useEffect</button>{' '}
+            <button onClick={() => { setLayout(false), setPreLayout(true) }}>useLayoutEffect</button>{' '}
+            <button onClick={() => { setLayout(null), setPreLayout(null) }}>clear</button>
+            <ExampleHook layout={layout} prelayout={prelayout} />
+        </div>
+    );
+}
+
+ExampleHook.propTypes = {
+    layout: PropTypes.bool,
+    prelayout: PropTypes.bool
+}
+
+ExampleHook.propsDefault = {
+    layout: false,
+    prelayout: false
+}
+
+
